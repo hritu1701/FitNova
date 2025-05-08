1e3cd8eafd566d74386b97b578d6b791f4ffceec
Hritu Raj <sac3xhin@gmail.com>
2025-05-08 11:48:47 +0530

    Initial Commit

---
+++ FitNova/FitNovaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitNovaApp.swift
+//  FitNova
+//
+//  Created by Hritu Raj on 08/05/25.
+//
+
+import SwiftUI
+
+@main
+struct FitNovaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
